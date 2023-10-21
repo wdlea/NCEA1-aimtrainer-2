4a324e98d5b7f8ba56435a037789436d9a9ebfc1
wdlea <83631101+wdlea@users.noreply.github.com>
2023-10-22 12:06:07 +1300

    Fixed bug caused by spamming escape.

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -9,7 +9,11 @@ public class PauseManager : MonoBehaviour
     private const int PAUSE_SCENE_INDEX = 2;
     [SerializeField] private int _menuSceneIndex;
 
+    public static bool IsPaused {get; private set;}
+
     public static void Pause(){
+        if(IsPaused) return;
+        IsPaused = true;
         Time.timeScale = 0;
         SceneManager.LoadScene(PAUSE_SCENE_INDEX, LoadSceneMode.Additive);
     }
@@ -17,12 +21,14 @@ public class PauseManager : MonoBehaviour
     public void Continue(){
         SceneManager.UnloadSceneAsync(PAUSE_SCENE_INDEX);
         Time.timeScale = 1;
+        IsPaused = false;
     }
 
     public async void Restart(){
         AsyncOperation op = SceneManager.UnloadSceneAsync(PAUSE_SCENE_INDEX);
         while(!op.isDone) await Task.Yield();
         Time.timeScale = 1;
+        IsPaused = false;
     }
 
     public async void MainMenu(){
@@ -30,5 +36,6 @@ public class PauseManager : MonoBehaviour
         AsyncOperation op = SceneManager.UnloadSceneAsync(PAUSE_SCENE_INDEX);
         while(!op.isDone) await Task.Yield();
         Time.timeScale = 1;
+        IsPaused = false;
     }
 }
