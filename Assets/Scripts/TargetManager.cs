96a04a97df1a578d0824c7b2928bb611288fc878
wdlea <83631101+wdlea@users.noreply.github.com>
2023-10-21 16:06:32 +1300

    Made targets increase in speed as time goes on

--- Assets/Scripts/TargetManager.cs
+++ Assets/Scripts/TargetManager.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class TargetManager : MonoBehaviour
 {
     [SerializeField] Target _targetPrefab;
-    [SerializeField] float _spawnInterval;
+
+    [SerializeField] float _spawnIntervalMultiplier;
+    [SerializeField] float _baseSpawnInterval;
+    float _currentSpawnInterval;
 
     // Start is called before the first frame update
     void Start()
     {
+        _currentSpawnInterval = _baseSpawnInterval;
         StartCoroutine(SpawnTargets());
     }
 
     IEnumerator SpawnTargets(){
         while(true){
             Instantiate(_targetPrefab);
-            yield return new WaitForSeconds(_spawnInterval);
+            yield return new WaitForSeconds(_currentSpawnInterval);
+
+            //last spawn interval becuase that is the "deltaTime" between this method getting called, so this makes the scaling consistent
+            _currentSpawnInterval *= Mathf.Pow(_spawnIntervalMultiplier, _currentSpawnInterval);
         }
     }
 }
