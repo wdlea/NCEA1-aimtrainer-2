cec78e8e79a18b8e735d1a5f36cc00be27d8d828
wdlea <83631101+wdlea@users.noreply.github.com>
2023-10-22 12:51:13 +1300

    Made stats display on the GameOver screen.

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -1,18 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
+using Packages.Rider.Editor.UnitTesting;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOverManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] StatisticDisplay _scoreText;
+    [SerializeField] StatisticDisplay _hitRateText;
+    [SerializeField] StatisticDisplay _timeSurvivedText;
+    [SerializeField] StatisticDisplay _targetCountText;
+    [SerializeField] StatisticDisplay _accuracyText;
+
+
+    void Start(){
+        _scoreText.DisplayStatistic(Statistics.Score);
+        _hitRateText.DisplayStatistic(Statistics.HitRate);
+        _timeSurvivedText.DisplayStatistic(Statistics.SurvivalTime);
+        _targetCountText.DisplayStatistic(Statistics.TargetsSpawned);
+        _accuracyText.DisplayStatistic(Statistics.Accuracy);
+    }
+}
+
+[Serializable] 
+public struct StatisticDisplay{
+    [SerializeField] Text _displayText;
+    [SerializeField] string _textPrefix;
+    [SerializeField] string _textSuffix;
+
+    public void DisplayStatistic(string text){
+        StringBuilder builder = new();
+
+        builder.Append(_textPrefix);
+        builder.Append(text);
+        builder.Append(_textSuffix);
+
+        _displayText.text = builder.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void DisplayStatistic(IFormattable value){
+        DisplayStatistic(value.ToString());
     }
 }
