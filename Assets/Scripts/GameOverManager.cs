26c79aa692aacdc5aeb2ebac3698249ec271fbed
wdlea <83631101+wdlea@users.noreply.github.com>
2023-10-22 13:16:44 +1300

    Fixed GameOver buttons.

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -1,27 +1,43 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Text;
-using Packages.Rider.Editor.UnitTesting;
-using Unity.VisualScripting;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameOverManager : MonoBehaviour
 {
+    public const int GAME_OVER_SCENE_INDEX = 3;
+    [SerializeField] private int _gameSceneIndex = 1;
+    [SerializeField] private int _menuSceneIndex = 0;
+
     [SerializeField] StatisticDisplay _scoreText;
     [SerializeField] StatisticDisplay _hitRateText;
     [SerializeField] StatisticDisplay _timeSurvivedText;
     [SerializeField] StatisticDisplay _targetCountText;
     [SerializeField] StatisticDisplay _accuracyText;
 
-
     void Start(){
+        Time.timeScale = 0;
         _scoreText.DisplayStatistic(Statistics.Score);
         _hitRateText.DisplayStatistic(Statistics.HitRate);
         _timeSurvivedText.DisplayStatistic(Statistics.SurvivalTime);
         _targetCountText.DisplayStatistic(Statistics.TargetsSpawned);
         _accuracyText.DisplayStatistic(Statistics.Accuracy);
+    }
+
+    public async void Restart(){
+        SceneManager.LoadScene(_gameSceneIndex);
+        AsyncOperation op = SceneManager.UnloadSceneAsync(GAME_OVER_SCENE_INDEX);
+        while(!op.isDone) await Task.Yield();
+        Time.timeScale = 1;
+    }
+    
+    public async void MainMenu(){
+        SceneManager.LoadScene(_menuSceneIndex);
+        AsyncOperation op = SceneManager.UnloadSceneAsync(GAME_OVER_SCENE_INDEX);
+        while(!op.isDone) await Task.Yield();
+        Time.timeScale = 1;
     }
 }
 
