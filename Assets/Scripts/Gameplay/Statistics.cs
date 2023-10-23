7a07d9134b40cf4299252309a0ae2ead50449c16
wdlea <83631101+wdlea@users.noreply.github.com>
2023-10-23 17:12:02 +1300

    Documented Statistics

--- Assets/Scripts/Gameplay/Statistics.cs
+++ Assets/Scripts/Gameplay/Statistics.cs
@@ -1,9 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// Keeps track of neccessary statistics
+/// </summary>
 public static class Statistics
 {
+
+    //calculate or store the various statistics
     public static int Score => TargetsHit - Misses;
     public static int TargetsSpawned {get; private set; }
     public static int TargetsHit {get; private set; }
@@ -12,6 +14,8 @@ public static class Statistics
     public static int HitRate => TargetsSpawned > 0 ? (int)((float)TargetsHit/TargetsSpawned * 100f) : 0;
     public static int Accuracy => ShotsFired > 0 ? (int)((float)TargetsHit/ShotsFired * 100f) : 0;
     public static int SurvivalTime => (int)Time.timeSinceLevelLoad;
+
+    //callbacks for updating some stats while not making their set public
 
     public static void OnHitTarget(){
         TargetsHit++;
