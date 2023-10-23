45a86b974ea8b31ded74e0ff8ec4a94972e7dd64
wdlea <83631101+wdlea@users.noreply.github.com>
2023-10-24 08:06:50 +1300

    Documented TimeElapsedCounter

--- Assets/Scripts/HUD/TimeElapsedCounter.cs
+++ Assets/Scripts/HUD/TimeElapsedCounter.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
+///<summary>
+/// Displays the currently elapsed time to the user
+///</summary>
 public class TimeElapsedCounter : MonoBehaviour
 {
+    //the formatting to the time elapsed
     [SerializeField] StatisticDisplay _display;
 
     // Update is called once per frame
     void Update()
     {
-        _display.DisplayStatistic(Statistics.SurvivalTime);
+        _display.DisplayStatistic(Statistics.SurvivalTime);//display the time elapsed
     }
 }
