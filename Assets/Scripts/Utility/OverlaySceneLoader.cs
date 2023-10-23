db6b3e3d2fe86fadcdbc0b8da50bad3f3e021494
wdlea <83631101+wdlea@users.noreply.github.com>
2023-10-24 08:28:55 +1300

    Documented OverlaySceneLoader

--- Assets/Scripts/Utility/OverlaySceneLoader.cs
+++ Assets/Scripts/Utility/OverlaySceneLoader.cs
@@ -1,17 +1,20 @@
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Handles loading the music scene
+/// </summary>
 public class OverlaySceneLoader : MonoBehaviour
 {
-    static HashSet<int> _loadedScenes = new();
+    static HashSet<int> _loadedScenes = new();//store the loaded scenes in a static set to determine what scenes are loaded
 
-    [SerializeField] int _sceneIndex;
+    [SerializeField] int _sceneIndex;//the scene index to load
     [SerializeField] bool _thereCanOnlyBeOne;
     // Start is called before the first frame update
     void Start()
     {
+        //only load if there are multiple allowed or the scene has not been loaded
         if(!_thereCanOnlyBeOne || !_loadedScenes.Contains(_sceneIndex))
             SceneManager.LoadScene(_sceneIndex, LoadSceneMode.Additive);//not using LoadSceneASync becuase that might create a data race if other loaders are allowed to load the same scene simultaneously, also becuase Unity's LoadSceneAsync doesn't support await
         _loadedScenes.Add(_sceneIndex);
