0c1f2b29b773711b7792eec17ddfb2c359aa9054
wdlea <83631101+wdlea@users.noreply.github.com>
2023-10-23 17:07:08 +1300

    Documented ScreenShaker

--- Assets/Scripts/Effects/ScreenShaker.cs
+++ Assets/Scripts/Effects/ScreenShaker.cs
@@ -2,13 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// It shakes the screen, as the name suggests.
+/// </summary>
 public class ScreenShaker : MonoBehaviour
 {
+    /// <summary>
+    /// The currently active instance of the screen shaker.
+    /// </summary>
     public static ScreenShaker Instance {get; private set;}
 
+    /// <summary>
+    /// A reference to the UI so it could shake that too if I enable it
+    /// </summary>
     [SerializeField] RectTransform _UI;
-    Camera _mainCamera;
 
+    /// <summary>
+    /// A reference to the target camera to shake.
+    /// </summary>
+    Camera _mainCamera;
+    
+    // Math stuff to determine the duration, intensity and number of shakes
     [SerializeField] float _shakeDurationBase = 1;
     [SerializeField] float _shakeDurationCoeffecient = 1;
     [SerializeField] float _shakeDistanceMultiplier = 0.1f;
@@ -17,29 +31,39 @@ public class ScreenShaker : MonoBehaviour
     [SerializeField] float _shakeClip = 0.1f;
 
     void Awake(){
+        //singleton pattern
         if(Instance != null)
-            Debug.LogWarning("More than 1 screen shaker in scene");
-        Instance = this;
+            Debug.LogWarning("More than 1 screen shaker in scene");//if there is more than 1 warn me
+        Instance = this;//regardless of ^ set the instance
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        _mainCamera = Camera.main;
-        StartCoroutine(DoShake());
+        _mainCamera = Camera.main;//Cache the main camera to avoid calling the underlying c++ code
+        StartCoroutine(DoShake());//start the shaker
     }
-
+    /// <summary>
+    /// The shake intensity, used to determine the speed, duration and number of the shakes.
+    /// </summary>
     public float CurrentShakeIntensity;
+
+    /// <summary>
+    /// The thing that does the shaking
+    /// </summary>
+    /// <returns>A coroutine to do the shaking.</returns>
     IEnumerator DoShake(){
-        float startTime = Time.realtimeSinceStartup;
+        float startTime = Time.realtimeSinceStartup;//keep track of the time at the start of the shake
         while(true){
-            yield return ShakeOnce();
+            yield return ShakeOnce();//shake once
 
             float currentTime = Time.realtimeSinceStartup;
+
+            //dampen the shake
             CurrentShakeIntensity *= Mathf.Pow(_shakeDamping, currentTime - startTime);
             startTime = currentTime;
 
-            if(CurrentShakeIntensity < _shakeClip) CurrentShakeIntensity = 0f;
+            if(CurrentShakeIntensity < _shakeClip) CurrentShakeIntensity = 0f;//if the shake is too low set it to 0
         }
     }
 
@@ -49,29 +73,31 @@ public class ScreenShaker : MonoBehaviour
             yield break;
         }
 
-        float shakeDistance = _shakeDistanceMultiplier * CurrentShakeIntensity;
+        float shakeDistance = _shakeDistanceMultiplier * CurrentShakeIntensity;//calculat the desired distance
 
         Vector3 startOffset = _mainCamera.transform.position;
         Vector3 finalOffset = new Vector3(
-            Random.Range(-shakeDistance, shakeDistance),
-            Random.Range(-shakeDistance, shakeDistance)
+            Random.Range(-shakeDistance, shakeDistance),//set a random position to go to that matches the desired distance
+            Random.Range(-shakeDistance, shakeDistance)//i could normalise and set the magnitude here but that is probably going to be slower becuase i am not using an expensive PRNG
         );
+        
+        float shakeDuration = _shakeDurationCoeffecient * Mathf.Pow(_shakeDurationBase, CurrentShakeIntensity);//calculate how long the shake should be
 
-        float shakeDuration = _shakeDurationCoeffecient * Mathf.Pow(_shakeDurationBase, CurrentShakeIntensity);
 
+        //initialise some values
         float startTime = Time.realtimeSinceStartup;
         float currentTime = startTime;
         float progress = 0;
         float endTime = startTime + shakeDuration;
         
-        while(currentTime < endTime){
-            Vector3 currentOffset = Vector3.Lerp(startOffset, finalOffset, progress);
+        while(currentTime < endTime){//for the duration of the shake
+            Vector3 currentOffset = Vector3.Lerp(startOffset, finalOffset, progress);// lerp between start and end positions by the current(normalised) time
 
             _mainCamera.transform.position = currentOffset + (10 * Vector3.back);
-            _UI.transform.position = currentOffset * _shakeUIMultiplier;
+            _UI.transform.position = currentOffset * _shakeUIMultiplier;///use these positions to determine the positions of the camera and the UI
 
-            yield return null;
-            currentTime = Time.realtimeSinceStartup;
+            yield return null;//wait a frame
+            currentTime = Time.realtimeSinceStartup;//recalculate the values on the next frame
             progress = (currentTime - startTime)/shakeDuration;
         }
     }
